5d46732431536a457346185510cb9143212f7cfe
Christian Bauer <christianbauer@Christians-MacBook-Pro.local>
2017-03-13 20:32:38 +0100

    Changed result field to TextView, also started with implementation of division

--- app/src/main/java/com/example/christianbauer/se_ii_dividierer/MainActivity.java
+++ app/src/main/java/com/example/christianbauer/se_ii_dividierer/MainActivity.java
@@ -2,12 +2,54 @@ package com.example.christianbauer.se_ii_dividierer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+	EditText nominator;
+	EditText denominator;
+	TextView result;
+	Button calculate;
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+
+
+		// Calculation starts here
+
+		denominator = (EditText) findViewById(R.id.denominator);
+		nominator = (EditText) findViewById(R.id.numerator);
+
+
+		calculate = (Button) findViewById(R.id.calculate);
+		calculate.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view){
+				double nomD = Double.parseDouble(nominator.getText().toString());
+				double denomD = Double.parseDouble(denominator.getText().toString());
+
+			}
+		});
 	}
+
+
+
+
+
+		// Handles the calculation + exceptions
+		protected double calculation (double nominator, double denominator) {
+			double result = 0.0;
+
+
+			return result;
+		}
+
+
 }
